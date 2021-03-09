da31f34fd5dd8386477bdd7c1a7b5b7111d96f5e
Vladimir Kazantsev <8406740@gmail.com>
2021-03-09 18:39:49 +0300

    Added zip for an array of Publishers.

--- CombineExtensions/Publisher+Extensions.swift
+++ CombineExtensions/Publisher+Extensions.swift
@@ -43,6 +43,27 @@ public extension Publisher {
 	func combinePrevious( initial: Output ) ->  Publishers.Scan<Self, ( Output, Output )> {
 		return self.scan( ( initial, initial ) ) { ( $0.1, $1 ) }
 	}
+
+	/// Attaches a subscriber with closure-based behavior.
+	///
+	/// Use ``Publisher/sink(receiveError:receiveValue:)`` to observe values received by the publisher and process them using a closure you specify.
+	///
+	/// This method creates the subscriber and immediately requests an unlimited number of values, prior to returning the subscriber.
+	/// Completion.finished events are discarded.
+	/// The return value should be held, otherwise the stream will be canceled.
+	///
+	/// - parameter receiveError: The closure to execute on error.
+	/// - parameter receiveValue: The closure to execute on receipt of a value.
+	/// - Returns: A cancellable instance, which you use when you end assignment of the received value. Deallocation of the result will tear down the subscription stream.
+	func sinkReceiveError( _ receiveError: @escaping ((Self.Failure) -> Void), receiveValue: @escaping ((Self.Output) -> Void)) -> AnyCancellable {
+		self.sink(
+			receiveCompletion: {
+				guard case .failure( let error ) = $0 else { return }
+				receiveError( error )
+			},
+			receiveValue: receiveValue
+		)
+	}
 }
 
 @available(OSX 10.15, iOS 13, tvOS 13, watchOS 6, *)
@@ -59,5 +80,37 @@ public extension Publisher where Self.Failure == Never {
 	func assignUnretained<Root: AnyObject>( to keyPath: ReferenceWritableKeyPath<Root, Self.Output>,
 												   on object: Root ) -> AnyCancellable {
 		self.sink { [weak object] in object?[ keyPath: keyPath ] = $0 }
+	}
+}
+
+
+@available(OSX 10.15, iOS 13, tvOS 13, watchOS 6, *)
+public extension Publishers {
+
+	/// A publisher created by applying the zip function to an array of upstream publishers.
+	struct Zip<Element, Failure: Error>: Publisher {
+		public typealias Output = [ Element ]
+		public typealias Failure = Failure
+
+		private let upstreams: [ AnyPublisher<Element, Failure> ]
+
+		public init( _ upstreams: [ AnyPublisher<Element, Failure> ] ) {
+			self.upstreams = upstreams
+		}
+
+		public func receive<S: Subscriber>( subscriber: S ) where Self.Failure == S.Failure, Self.Output == S.Input {
+			let initial = Just<[Element]>([])
+				.setFailureType( to: Failure.self )
+				.eraseToAnyPublisher()
+
+			let zipped = upstreams
+				.reduce( initial ) { result, upstream in
+					result
+						.zip( upstream ) { $0 + [ $1 ] }
+						.eraseToAnyPublisher()
+			}
+
+			zipped.subscribe( subscriber )
+		}
 	}
 }
---
+++ CombineExtensionsTests/PublishersExtensionsTests.swift
@@ -0,0 +1,72 @@
+//
+//  PublishersExtensionsTests.swift
+//  CombineExtensionsTests
+//
+//  Created by Vladimir Kazantsev on 09.03.2021.
+//  Copyright © 2021 MC2 Software. All rights reserved.
+//
+
+import XCTest
+import Combine
+import CombineExtensions
+
+class PublishersExtensionsTests: XCTestCase {
+
+	var cancellable: AnyCancellable?
+
+	func createPublisher( delay milliseconds: Int ) -> AnyPublisher<Int, Error> {
+		Just( milliseconds )
+			.delay( for: .milliseconds( milliseconds ), scheduler: RunLoop.main )
+			.setFailureType( to: Error.self )
+			.eraseToAnyPublisher()
+	}
+
+	func testZipPublishersArray() {
+
+		let completionExpectation = expectation( description: "Completion has been received" )
+		let resultExpectation = expectation( description: "Result has been received" )
+
+		cancellable = Publishers.Zip( [
+			createPublisher(delay: 1000),
+			createPublisher(delay: 20),
+			createPublisher(delay: 1500),
+			createPublisher(delay: 1200)
+		])
+		.sink( receiveCompletion: {
+			if case .finished = $0 { completionExpectation.fulfill() }
+			else { XCTAssert( false, "Received unexpected error" ) }
+		}, receiveValue: { results in
+			XCTAssertEqual( results, [ 1000, 20, 1500, 1200 ] )
+			resultExpectation.fulfill()
+		})
+
+		wait( for: [ resultExpectation, completionExpectation ], timeout: 2000 )
+	}
+
+	func testZipPublishersArrayFailure() {
+
+		let failureExpectation = expectation( description: "Error has been received" )
+
+		let failurePublisher = Just( 200 )
+			.delay( for: .milliseconds( 200 ), scheduler: RunLoop.main )
+			.setFailureType( to: Error.self )
+			.flatMap { _ in Fail<Int, Error>(error: NSError( domain: "", code: -1, userInfo: nil )) }
+			.eraseToAnyPublisher()
+
+
+		cancellable = Publishers.Zip( [
+			createPublisher( delay: 1000 ),
+			failurePublisher,
+			createPublisher( delay: 1500 ),
+			createPublisher( delay: 1200 )
+		])
+		.sink( receiveCompletion: {
+			if case .finished = $0 { XCTAssert( false, "Received unexpected completion" ) }
+			else { failureExpectation.fulfill() }
+		}, receiveValue: { results in
+			XCTAssert( false, "Received unexpected result" )
+		})
+
+		wait( for: [ failureExpectation ], timeout: 300 )
+	}
+}
